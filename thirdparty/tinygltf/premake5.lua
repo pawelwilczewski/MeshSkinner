fff01e024a1d8038c8889c5bb6e127140fd3dccc
PawelWilczewski <pawel.wilczewski@outlook.com>
2023-02-03 02:53:26 +0000

    Update premake5.lua

--- thirdparty/tinygltf/premake5.lua
+++ thirdparty/tinygltf/premake5.lua
@@ -15,23 +15,3 @@ project "tinygltf"
 		"stb_image_write.h",
 		"json.hpp"
 	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
