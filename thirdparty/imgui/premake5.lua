c1e428b69b3e7451bc518eebf9be195bb3364546
PawelWilczewski <pawel.wilczewski@outlook.com>
2023-02-03 02:51:45 +0000

    Updated glad and imgui premake accordingly

--- thirdparty/imgui/premake5.lua
+++ thirdparty/imgui/premake5.lua
@@ -20,23 +20,3 @@ project "imgui"
 		"imstb_truetype.h",
 		"imgui_demo.cpp"
 	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-    filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
