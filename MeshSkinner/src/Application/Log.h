03b55032e4792a5b4510d358aba4becd1e6ae90a
PawelWilczewski <pawel.wilczewski@outlook.com>
2023-02-03 07:26:29 +0000

    Simplified logger

--- MeshSkinner/src/Application/Log.h
+++ MeshSkinner/src/Application/Log.h
@@ -11,13 +11,11 @@ private:
 	static void Init();
 
 public:
-	static std::shared_ptr<spdlog::logger> &GetLogger() { return s_Logger; }
-
-	template<typename... Args> static void Trace(const char *format, Args &&... args) { GetLogger()->trace(format, std::forward<Args>(args)...); }
-	template<typename... Args> static void Info(const char *format, Args &&... args) { GetLogger()->info(format, std::forward<Args>(args)...); }
-	template<typename... Args> static void Warn(const char *format, Args &&... args) { GetLogger()->warn(format, std::forward<Args>(args)...); }
-	template<typename... Args> static void Error(const char *format, Args &&... args) { GetLogger()->error(format, std::forward<Args>(args)...); }
-	template<typename... Args> static void Critical(const char *format, Args &&... args) { GetLogger()->critical(format, std::forward<Args>(args)...); }
+	template<typename... Args> static void Trace(const char *format, Args &&... args) { s_Logger->trace(format, std::forward<Args>(args)...); }
+	template<typename... Args> static void Info(const char *format, Args &&... args) { s_Logger->info(format, std::forward<Args>(args)...); }
+	template<typename... Args> static void Warn(const char *format, Args &&... args) { s_Logger->warn(format, std::forward<Args>(args)...); }
+	template<typename... Args> static void Error(const char *format, Args &&... args) { s_Logger->error(format, std::forward<Args>(args)...); }
+	template<typename... Args> static void Critical(const char *format, Args &&... args) { s_Logger->critical(format, std::forward<Args>(args)...); }
 
 private:
 	static std::shared_ptr<spdlog::logger> s_Logger;
