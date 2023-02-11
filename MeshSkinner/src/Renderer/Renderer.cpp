f70417e7061688484f5c3accc110d886f984b4ab
PawelWilczewski <pawel.wilczewski@outlook.com>
2023-02-11 13:57:18 +0000

    Submit static meshes template function

--- MeshSkinner/src/Renderer/Renderer.cpp
+++ MeshSkinner/src/Renderer/Renderer.cpp
@@ -22,59 +22,8 @@ void Renderer::Init()
 
 void Renderer::Submit(Ref<Entity> entity)
 {
-	// add the static static meshes
-	auto staticMeshes = entity->GetComponents<StaticMesh>();
-	for (const auto &mesh : staticMeshes)
-	{
-		if (!mesh->isStatic) continue;
-		
-		uint32_t indexOffset;
-		// the mesh is already rendered - reuse its data
-		if (meshes.find(mesh) != meshes.end())
-		{
-			// reuse the cached index offset
-			indexOffset = meshes[mesh];
-		}
-		// new mesh
-		else
-		{
-			// new shader - create an appropriate vao
-			if (staticMeshStaticDrawCalls.find(mesh->material->shader) == staticMeshStaticDrawCalls.end())
-			{
-				auto ibo = MakeRef<IndexBuffer<uint32_t>>();
-				auto vbo = MakeRef<VertexBuffer<StaticVertex>>(StaticVertex::layout);
-				auto vao = MakeRef<VertexArray<uint32_t>>();
-				vao->SetVertexBuffer(vbo, 0);
-				vao->SetIndexBuffer(ibo);
-				staticMeshStaticDrawCalls.insert({ mesh->material->shader, vao });
-			}
-
-			// append the data to the vbo and ibo of the vao for the current shader
-			auto vbo = TypedVB<StaticVertex>(staticMeshStaticDrawCalls[mesh->material->shader]->GetVertexBuffer(0).get());
-
-			// cache the current vbo length for ibo offset
-			indexOffset = vbo->GetLength();
-
-			// vbo - add vertices
-			vbo->SetData(mesh->vertices.data(), mesh->vertices.size(), vbo->GetLength());
-
-			// add the meshes to the map for reuse later
-			meshes.insert({ mesh, indexOffset });
-		}
-
-		auto &ibo = staticMeshStaticDrawCalls[mesh->material->shader]->GetIndexBuffer();
-
-		// add indices (appropriately offset)
-		std::vector<uint32_t> indicesOffset = mesh->indices;
-		if (indexOffset > 0)
-			for (auto &index : indicesOffset)
-				index += indexOffset;
-
-		ibo->SetData(indicesOffset.data(), indicesOffset.size(), ibo->GetLength());
-	}
-
-	// add the static skeletal meshes
-	//auto skeletalMeshes = entity->GetComponents<SkeletalMesh>();
+	SubmitStatic<StaticMesh, StaticVertex>(entity->GetComponents<StaticMesh>(), staticMeshStaticDrawCalls, StaticVertex::layout);
+	SubmitStatic<SkeletalMesh, SkeletalVertex>(entity->GetComponents<SkeletalMesh>(), skeletalMeshStaticDrawCalls, SkeletalVertex::layout);
 }
 
 void Renderer::FrameBegin()
--- MeshSkinner/src/Renderer/Renderer.h
+++ MeshSkinner/src/Renderer/Renderer.h
@@ -18,6 +18,59 @@ public:
 	static void FrameBegin();
 	static void FrameEnd();
 
+private:
+	template <typename M, typename V>
+	static void SubmitStatic(const std::unordered_set<M *> &submitMeshes, std::unordered_map<Ref<Shader>, Ref<VertexArray<uint32_t>>> &drawCalls, const BufferLayout &vertexLayout)
+	{
+		for (const auto &mesh : submitMeshes)
+		{
+			if (!mesh->isStatic) continue;
+
+			uint32_t indexOffset;
+			// the mesh is already rendered - reuse its data
+			if (meshes.find(mesh) != meshes.end())
+			{
+				// reuse the cached index offset
+				indexOffset = meshes[mesh];
+			}
+			// new mesh
+			else
+			{
+				// new shader - create an appropriate vao
+				if (drawCalls.find(mesh->material->shader) == drawCalls.end())
+				{
+					auto ibo = MakeRef<IndexBuffer<uint32_t>>();
+					auto vbo = MakeRef<VertexBuffer<V>>(vertexLayout);
+					auto vao = MakeRef<VertexArray<uint32_t>>();
+					vao->SetVertexBuffer(vbo, 0);
+					vao->SetIndexBuffer(ibo);
+					drawCalls.insert({ mesh->material->shader, vao });
+				}
+
+				// append the data to the vbo and ibo of the vao for the current shader
+				auto vbo = TypedVB<V>(drawCalls[mesh->material->shader]->GetVertexBuffer(0).get());
+
+				// cache the current vbo length for ibo offset
+				indexOffset = vbo->GetLength();
+
+				// vbo - add vertices
+				vbo->SetData(mesh->vertices.data(), mesh->vertices.size(), vbo->GetLength());
+
+				// add the meshes to the map for reuse later
+				meshes.insert({ mesh, indexOffset });
+			}
+
+			// add indices (appropriately offset)
+			std::vector<uint32_t> indicesOffset = mesh->indices;
+			if (indexOffset > 0)
+				for (auto &index : indicesOffset)
+					index += indexOffset;
+
+			auto &ibo = drawCalls[mesh->material->shader]->GetIndexBuffer();
+			ibo->SetData(indicesOffset.data(), indicesOffset.size(), ibo->GetLength());
+		}
+	}
+
 public:
 	static Ref<Camera> activeCamera;
 
