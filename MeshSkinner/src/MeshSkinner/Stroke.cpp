59b3fbd19881616a619e0899e3f893113dbaf965
PawelWilczewski <pawel.wilczewski@outlook.com>
2023-03-03 18:28:45 +0000

    Stroke UI implementation

--- MeshSkinner/src/MeshSkinner/Stroke.cpp
+++ MeshSkinner/src/MeshSkinner/Stroke.cpp
@@ -37,32 +37,25 @@ void Stroke::OnUpdateUI()
 {
 	ImGui::Begin(toolWindowName.c_str());
 
-	const char *items[] = { "PixelDistance", "WorldDistance", "EachFrame", "FixedDeltaTime" }; // TODO: obviously do not have it fixed here like that
+	const char *items[] = { "Pixel Distance", "World Distance", "Each Frame", "Fixed Delta Time" }; // TODO: obviously do not have it fixed here like that
 	InteractiveWidget(ImGui::ListBox("Stroke update type", (int *)(&type), items, 4));
 
-	//InteractiveWidget(ImGui::SliderFloat("Brush weight", &weight, 0.f, 1.f, "%.3f", ImGuiSliderFlags_ClampOnInput));
 	switch (type)
 	{
 	case Type::PixelDistance:
-		float pixelDistance;
-		float worldDistance;
-		float fixedIntervalTime;
+		InteractiveWidget(ImGui::DragFloat("Pixel distance", &pixelDistance, 1.f, 0.f, 10000.f, "%.3f", ImGuiSliderFlags_ClampOnInput | ImGuiSliderFlags_Logarithmic));
 		break;
 	case Type::WorldDistance:
+		InteractiveWidget(ImGui::DragFloat("World distance", &worldDistance, 1.f, 0.f, 10000.f, "%.3f", ImGuiSliderFlags_ClampOnInput | ImGuiSliderFlags_Logarithmic));
 		break;
 	case Type::EachFrame:
 		break;
 	case Type::FixedDeltaTime:
+		InteractiveWidget(ImGui::DragFloat("Fixed delta time", &pixelDistance, 0.001f, 0.f, 1.f, "%.3f", ImGuiSliderFlags_ClampOnInput | ImGuiSliderFlags_Logarithmic));
 		break;
 	default:
 		assert(false);
 	}
-
-	//if (type ::Mix || blendMode == BlendMode::Linear)
-	//	interacting |= ImGui::SliderFloat("Brush strength", &strength, 0.f, 1.f, "%.3f", ImGuiSliderFlags_ClampOnInput);
-
-	//interacting |= ImGui::DragFloat("Brush radius", &radius, 1.f, 0.f, 10000.f, "%.3f", ImGuiSliderFlags_ClampOnInput | ImGuiSliderFlags_Logarithmic);
-	//interacting |= ImGui::DragFloat("Brush falloff", &falloff, 0.01f, 0.f, 10.f, "%.3f", ImGuiSliderFlags_ClampOnInput);
 
 	ImGui::End();
 }
